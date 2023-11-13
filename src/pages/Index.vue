eb2ae3b953001a0200270b6dc47bcf27715e42ff
amani esseili <esseiliamani@hotmail.com>
2023-11-13 22:20:56 +0100

    Updated select element style 4 enchanced visual appeal

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -66,7 +66,9 @@ export default {
     <div class="row">
       <div class="col-12 col-lg-4 categories-bg text-center">
         <header class="header">
-          <strong><h1 class="title">Cyber Security</h1></strong>
+          <strong>
+            <h1 class="title">Cyber Security</h1>
+          </strong>
 
           <p class="description ps-5 pe-5">
             Cyber Security è il punto di riferimento per esperti di sicurezza
@@ -80,42 +82,7 @@ export default {
           </p>
           <router-link to="/" class="btn">Torna Indietro</router-link>
         </header>
-        <div class="row flex-column justify-content-center p-5">
-          <div class="col-4 pb-3">
-            <label for="minAverageScore" class="form-label"
-              >Seleziona Media Voti:</label
-            >
-            <select
-              v-model="minAverageScore"
-              @change="fetchDataProfile"
-              class="form-select"
-            >
-              <option value="0">Mostra tutti</option>
-              <option
-                v-for="score in [1, 2, 3, 4, 5]"
-                :key="score"
-                :value="score"
-              >
-                {{ score }}
-              </option>
-            </select>
-          </div>
-          <div class="col-4">
-            <label for="numeroRecensioni" class="form-label"
-              >Seleziona utenti con più di:</label
-            >
-            <select
-              v-model="minReviewCount"
-              @change="fetchDataProfile"
-              class="form-select"
-            >
-              <option value="0">Mostra tutti</option>
-              <option v-for="count in [1, 5, 10]" :key="count" :value="count">
-                {{ count }} recensioni
-              </option>
-            </select>
-          </div>
-        </div>
+
       </div>
 
       <div class="col-lg-8 col-12">
@@ -128,10 +95,33 @@ export default {
               <h4 class="slogan-font-color text-end mb-5">
                 Il Tuo Scudo Digitale nel Mondo Virtuale
               </h4>
-              <div class="text-end">
-                <router-link to="/" class="btn"
+              <div class="text-start">
+                <!-- -----------select------------ -->
+                <div class="row ">
+                  <div class="col-2"></div>
+                  <div class="col-5 pb-3">
+                    <label for="minAverageScore" class="form-label info">Votazione Media</label>
+                    <select v-model="minAverageScore" @change="fetchDataProfile" class="form-select select-bg">
+                      <option class="select-color" value="0">Mostra tutti</option>
+                      <option class="select-color" v-for=" score in [1, 2, 3, 4, 5]" :key="score" :value="score">
+                        {{ score }}
+                      </option>
+                    </select>
+                  </div>
+                  <div class="col-5 ">
+                    <label for="numeroRecensioni" class="form-label info">Recensioni Multiple</label>
+                    <select v-model="minReviewCount" @change="fetchDataProfile" class="form-select select-bg">
+                      <option class="select-color" value="0">Mostra tutti</option>
+                      <option class="select-color" v-for="count in [1, 5, 10]" :key="count" :value="count">
+                        {{ count }} recensioni
+                      </option>
+                    </select>
+                  </div>
+                </div>
+                <!-- ------------------------------ -->
+                <!-- <router-link to="/" class="btn"
                   >Controlla Disponibilita</router-link
-                >
+                > -->
               </div>
             </header>
           </div>
@@ -142,21 +132,13 @@ export default {
         <!-- Visualizza le card degli utenti -->
         <div class="row scrolling-container">
           <div class="col-md-4" v-for="user in profiles" :key="user.id">
-            <div
-              class="card scroll-card bg-card mb-4"
-              v-if="
-                !isNaN(user.average_score) &&
-                user.average_score >= minAverageScore &&
-                !isNaN(user.review_count) &&
-                user.review_count >= minReviewCount
-              "
-            > 
-              <img
-                v-if="user.profile && user.profile.photo"
-                :src="getImageUrl(user.profile.photo)"
-                class="card-img-top"
-                alt="Immagine Professionista"
-              />
+            <div class="card scroll-card bg-card mb-4" v-if="!isNaN(user.average_score) &&
+              user.average_score >= minAverageScore &&
+              !isNaN(user.review_count) &&
+              user.review_count >= minReviewCount
+              ">
+              <img v-if="user.profile && user.profile.photo" :src="getImageUrl(user.profile.photo)" class="card-img-top"
+                alt="Immagine Professionista" />
               <div class="card-body overflow-auto  flex-column p-1">
                 <h5 class="card-title info text-center mb-2">{{ user.name }} {{ user.surname }}</h5>
                 <div class="details " v-if="user.profile">
@@ -173,23 +155,22 @@ export default {
                   <div class="card-text mb-3">
                     <p class="info">Location: <span class="description">{{ user.profile.location }} </span> </p>
                     <p class="info">Skills: <span class="description">{{ user.profile.skills }}</span> </p>
-                    <p class="info" style="word-wrap: break-word">Description: <span class="description"> {{ user.profile.description }}</span></p>
+                    <p class="info" style="word-wrap: break-word">Description: <span class="description"> {{
+                      user.profile.description }}</span></p>
                     <p class="info">
                       Specialization: <span class="description">
-                      {{
-                        user.specializations
-                          .map((specialization) => specialization.name)
-                          .join(", ")
-                      }}</span>
+                        {{
+                          user.specializations
+                            .map((specialization) => specialization.name)
+                            .join(", ")
+                        }}</span>
                     </p>
-                    <router-link class="link" :to="{ name: 'show', params: { id: user.id } }"
-                      >Dettagli</router-link
-                    >
+                    <router-link class="link" :to="{ name: 'show', params: { id: user.id } }">Dettagli</router-link>
                   </div>
                 </div>
               </div>
-              
-              
+
+
             </div>
           </div>
         </div>
@@ -203,56 +184,62 @@ export default {
 
 <style lang="scss" scoped>
 
-.info{
+
+.info {
   font-size: 1rem;
   color: #27CDF2;
   font-weight: bold
+}
+
+body {
+  background-image: url(/bg-2.jpg);
+  background-size: cover;
+  background-attachment: fixed; //fissa il bg-img per evitare lo scrolling
+  background-repeat: no-repeat;
 
 }
 
-body{
-	background-image: url(/bg-2.jpg);
-	background-size: cover;
-	background-attachment: fixed; //fissa il bg-img per evitare lo scrolling
-	background-repeat: no-repeat;
-
-}
 /* aggiunge un overlay trasparente all'immagine di sfondo */
 body::before {
   content: "";
-  background: rgba(173, 171, 171, 0.5); 
+  background: rgba(173, 171, 171, 0.5);
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  z-index: -1; /* Posiziona l'overlay dietro all'immagine di sfondo */
+  z-index: -1;
+  /* Posiziona l'overlay dietro all'immagine di sfondo */
 }
 
 .categories-bg {
-	background-color: rgba(51, 51, 51, 0.9);
-	color: #fff;
-	height: 100vh;
+  background-color: rgba(51, 51, 51, 0.9);
+  color: #fff;
+  height: 100vh;
 }
 
 
 /* ----------------------------------------- */
-.title{
-	font-size: 4rem;
-	color: #27CDF2;
+.title {
+  font-size: 4rem;
+  color: #27CDF2;
 
 }
+
 .description {
   //color: white;
   color: #b0b1b2;
 }
+
 .slogan {
   color: #27cdf2;
   margin-bottom: 0;
 }
+
 .slogan h4 {
   margin-top: -1.5rem;
 }
+
 .slogan-font-color {
   color: #b0b1b2;
 }
@@ -287,20 +274,48 @@ body::before {
 }
 
 .scroll-card {
-  height: 350px; /* Imposta l'altezza fissa del contenitore */
+  height: 350px;
+  /* Imposta l'altezza fissa del contenitore */
 }
 
 .bg-card {
   background-color: rgba(51, 51, 51, 0.5);
   color: #b0b1b2;
 }
+
 .card-body {
-  
+
   display: flex;
   flex-direction: row;
   justify-content: space-between;
 }
-.link{
+
+.link {
+  color: #b0b1b2;
+}
+.link:hover{
+  background-color: rgba(51, 51, 51, 0.9) ;
+  padding: 0.5rem;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  border-radius: 8px;
   color: #27CDF2;
+}
+/*---------select--------*/
+.select-bg{
+  background-color: rgba(51, 51, 51, 0.5);
+  color: rgb(173, 171, 171, );
+  border: 1px solid grey;
+}
+.select-color{
+  color: #27CDF2;
+  background-color: rgba(39, 39, 39, 0.5);
+}
+.bg-select{
+  background-color:rgba(51, 51, 51, 0.9);
+  
+}
+option{
+  background-color: rgba(85, 84, 84, 0.9);
 }
 </style>
